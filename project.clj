f06ab0e5f921b0705bc4e78ace5f3ab4f9516aa5
Joakim L. Engeset <joakim.engeset@gmail.com>
2021-09-12 18:34:35 +0200

    Add toml and gmail support

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject clj-gmail "0.1.0-SNAPSHOT"
   :description "Read Gmail Inbox using Clojure"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [io.forward/clojure-mail "1.0.8"]
+                 [toml "0.1.3"]]
   :main ^:skip-aot clj-gmail.core
   :profiles {:uberjar {:aot :all :uberjar-name "app.jar"}})
